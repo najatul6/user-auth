fe8e85e86457c6e916a0a686f3c94cc880f86ac4
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-09-15 23:03:42 +0600

    fix some error in login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -7,7 +7,7 @@ const Login = () => {
           <form>
             <div className="mb-12">
               <h3 className="text-gray-800 text-3xl font-extrabold">Sign in</h3>
-              <p className="text-sm mt-4 text-gray-800">Don't have an account <a href="javascript:void(0);" className="text-blue-600 font-semibold hover:underline ml-1 whitespace-nowrap">Register here</a></p>
+              <p className="text-sm mt-4 text-gray-800">Don&#39;t have an account <a href="javascript:void(0);" className="text-blue-600 font-semibold hover:underline ml-1 whitespace-nowrap">Register here</a></p>
             </div>
 
             <div>
@@ -20,8 +20,8 @@ const Login = () => {
                       <path d="M0 512h512V0H0Z" data-original="#000000"></path>
                     </clipPath>
                   </defs>
-                  <g clip-path="url(#a)" transform="matrix(1.33 0 0 -1.33 0 682.667)">
-                    <path fill="none" stroke-miterlimit="10" stroke-width="40" d="M452 444H60c-22.091 0-40-17.909-40-40v-39.446l212.127-157.782c14.17-10.54 33.576-10.54 47.746 0L492 364.554V404c0 22.091-17.909 40-40 40Z" data-original="#000000"></path>
+                  <g clipPath="url(#a)" transform="matrix(1.33 0 0 -1.33 0 682.667)">
+                    <path fill="none" strokeMiterlimit="10" strokeWidth="40" d="M452 444H60c-22.091 0-40-17.909-40-40v-39.446l212.127-157.782c14.17-10.54 33.576-10.54 47.746 0L492 364.554V404c0 22.091-17.909 40-40 40Z" data-original="#000000"></path>
                     <path d="M472 274.9V107.999c0-11.027-8.972-20-20-20H60c-11.028 0-20 8.973-20 20V274.9L0 304.652V107.999c0-33.084 26.916-60 60-60h392c33.084 0 60 26.916 60 60v196.653Z" data-original="#000000"></path>
                   </g>
                 </svg>
@@ -41,7 +41,7 @@ const Login = () => {
             <div className="flex flex-wrap items-center justify-between gap-4 mt-6">
               <div className="flex items-center">
                 <input id="remember-me" name="remember-me" type="checkbox" className="h-4 w-4 shrink-0 text-blue-600 focus:ring-blue-500 border-gray-300 rounded" />
-                <label for="remember-me" className="ml-3 block text-sm text-gray-800">
+                <label htmlFor="remember-me" className="ml-3 block text-sm text-gray-800">
                   Remember me
                 </label>
               </div>

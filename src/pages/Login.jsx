5b36bc584fbb3e6a80970fb0388bd9283b307631
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-09-15 23:23:05 +0600

    change text color and background color

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,19 +1,19 @@
 const Login = () => {
   return (
     <div>
-    <div className="min-h-screen flex flex-col items-center justify-center">
+    <div className="min-h-screen flex flex-col bg-base-300 items-center justify-center">
       <div className="grid md:grid-cols-2 items-center gap-4 max-md:gap-8 max-w-6xl max-md:max-w-lg w-full p-4 m-4 shadow-[0_2px_10px_-3px_rgba(6,81,237,0.3)] rounded-md">
         <div className="md:max-w-md w-full px-4 py-4">
           <form>
             <div className="mb-12">
-              <h3 className="text-gray-800 text-3xl font-extrabold">Sign in</h3>
-              <p className="text-sm mt-4 text-gray-800">Don&#39;t have an account <a href="javascript:void(0);" className="text-blue-600 font-semibold hover:underline ml-1 whitespace-nowrap">Register here</a></p>
+              <h3 className=" text-3xl font-extrabold">Sign in</h3>
+              <p className="text-sm mt-4 ">Don&#39;t have an account <a href="javascript:void(0);" className="text-blue-600 font-semibold hover:underline ml-1 whitespace-nowrap">Register here</a></p>
             </div>
 
             <div>
-              <label className="text-gray-800 text-xs block mb-2">Email</label>
+              <label className=" text-xs block mb-2">Email</label>
               <div className="relative flex items-center">
-                <input name="email" type="text" required className="w-full text-gray-800 text-sm border-b border-gray-300 focus:border-blue-600 px-2 py-3 outline-none" placeholder="Enter email" />
+                <input name="email" type="text" required className="w-full  text-sm border-b border-gray-300 focus:border-blue-600 px-2 py-3 outline-none" placeholder="Enter email" />
                 <svg xmlns="http://www.w3.org/2000/svg" fill="#bbb" stroke="#bbb" className="w-[18px] h-[18px] absolute right-2" viewBox="0 0 682.667 682.667">
                   <defs>
                     <clipPath id="a" clipPathUnits="userSpaceOnUse">
@@ -29,9 +29,9 @@ const Login = () => {
             </div>
 
             <div className="mt-8">
-              <label className="text-gray-800 text-xs block mb-2">Password</label>
+              <label className=" text-xs block mb-2">Password</label>
               <div className="relative flex items-center">
-                <input name="password" type="password" required className="w-full text-gray-800 text-sm border-b border-gray-300 focus:border-blue-600 px-2 py-3 outline-none" placeholder="Enter password" />
+                <input name="password" type="password" required className="w-full  text-sm border-b border-gray-300 focus:border-blue-600 px-2 py-3 outline-none" placeholder="Enter password" />
                 <svg xmlns="http://www.w3.org/2000/svg" fill="#bbb" stroke="#bbb" className="w-[18px] h-[18px] absolute right-2 cursor-pointer" viewBox="0 0 128 128">
                   <path d="M64 104C22.127 104 1.367 67.496.504 65.943a4 4 0 0 1 0-3.887C1.367 60.504 22.127 24 64 24s62.633 36.504 63.496 38.057a4 4 0 0 1 0 3.887C126.633 67.496 105.873 104 64 104zM8.707 63.994C13.465 71.205 32.146 96 64 96c31.955 0 50.553-24.775 55.293-31.994C114.535 56.795 95.854 32 64 32 32.045 32 13.447 56.775 8.707 63.994zM64 88c-13.234 0-24-10.766-24-24s10.766-24 24-24 24 10.766 24 24-10.766 24-24 24zm0-40c-8.822 0-16 7.178-16 16s7.178 16 16 16 16-7.178 16-16-7.178-16-16-16z" data-original="#000000"></path>
                 </svg>
@@ -41,7 +41,7 @@ const Login = () => {
             <div className="flex flex-wrap items-center justify-between gap-4 mt-6">
               <div className="flex items-center">
                 <input id="remember-me" name="remember-me" type="checkbox" className="h-4 w-4 shrink-0 text-blue-600 focus:ring-blue-500 border-gray-300 rounded" />
-                <label htmlFor="remember-me" className="ml-3 block text-sm text-gray-800">
+                <label htmlFor="remember-me" className="ml-3 block text-sm ">
                   Remember me
                 </label>
               </div>

8810c9b0448b899c6fa837462a853225779aa0c6
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-09-15 23:07:40 +0600

    Add footer in root

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 import Headers from "../components/Headers";
+import Footer from "../components/Footer";
 
 const Layout = () => {
   return (
-    <div className="text-white">
+    <div className="">
         <Headers/>
       <Outlet />
+      <Footer/>
     </div>
   );
 };

606bdef8c74d920686b9188ac25b06ab31609bee
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-09-16 04:44:05 +0600

    upgrade section added

---
+++ src/components/Upgrade.jsx
@@ -0,0 +1,21 @@
+
+const Upgrade = () => {
+  return (
+    <div className="bg-[#182b50] px-8 py-16 font-[sans-serif]">
+      <div className="max-w-6xl mx-auto grid md:grid-cols-2 justify-center items-center gap-12">
+        <div className="text-center md:text-left">
+          <h2 className="text-4xl lg:text-5xl font-extrabold text-white mb-6 md:!leading-[55px]">Unlock Premium Features</h2>
+          <p className="text-lg lg:text-xl text-white">Upgrade to our premium plan and supercharge your experience. Don&39;t miss out!</p>
+          <a href="jacascrip:void(0);" className="mt-12 bg-[#a91079] hover:bg-opacity-80 text-white py-3 px-6 rounded-full text-lg lg:text-xl transition duration-300 ease-in-out transform hover:scale-105 hover:shadow-xl inline-block">
+            Upgrade Now
+          </a>
+        </div>
+        <div className="text-center">
+          <img src="https://readymadeui.com/management-img.webp" alt="Premium Benefits" className="w-full mx-auto" />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Upgrade
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,11 +2,13 @@ import Banner from "../components/Banner";
 import Partnership from "../components/Partnership";
 import Pricing from "../components/Pricing";
 import Testimonial from "../components/Testimonial";
+import Upgrade from "../components/Upgrade";
 
 const Home = () => {
   return (
     <div>
       <Banner />
+      <Upgrade/>
       <Testimonial />
       <Pricing/>
       <Partnership />

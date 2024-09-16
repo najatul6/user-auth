e5d93d332e9309b7be07ee900cf45a1067598e54
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-09-16 07:17:55 +0600

    Password hide and show function now worked

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,6 +1,9 @@
+import { useState } from "react";
+import { IoEye, IoEyeOff } from "react-icons/io5";
 import { Link } from "react-router-dom";
 
 const Register = () => {
+  const [isShow, setIsShow] = useState(false);
   const handleSubmit = (e) => {
     e.preventDefault();
     const form = e.target;
@@ -180,12 +183,18 @@ const Register = () => {
                 <div className="relative flex items-center">
                   <input
                     name="password"
-                    type="password"
+                    type={`${isShow ? "text" : "password"}`}
                     required
                     className="bg-white border border-gray-300 w-full text-sm text-[#FFA726] pl-4 pr-10 py-2.5 rounded-md outline-blue-500"
                     placeholder="Enter password"
                   />
-                  <svg
+                  <a
+                    onClick={() => setIsShow(!isShow)}
+                    className="absolute right-4 cursor-pointer"
+                  >
+                    {isShow ? <IoEye /> : <IoEyeOff />}
+                  </a>
+                  {/* <svg
                     xmlns="http://www.w3.org/2000/svg"
                     fill="#bbb"
                     stroke="#bbb"
@@ -196,7 +205,7 @@ const Register = () => {
                       d="M64 104C22.127 104 1.367 67.496.504 65.943a4 4 0 0 1 0-3.887C1.367 60.504 22.127 24 64 24s62.633 36.504 63.496 38.057a4 4 0 0 1 0 3.887C126.633 67.496 105.873 104 64 104zM8.707 63.994C13.465 71.205 32.146 96 64 96c31.955 0 50.553-24.775 55.293-31.994C114.535 56.795 95.854 32 64 32 32.045 32 13.447 56.775 8.707 63.994zM64 88c-13.234 0-24-10.766-24-24s10.766-24 24-24 24 10.766 24 24-10.766 24-24 24zm0-40c-8.822 0-16 7.178-16 16s7.178 16 16 16 16-7.178 16-16-7.178-16-16-16z"
                       data-original="#000000"
                     ></path>
-                  </svg>
+                  </svg> */}
                 </div>
               </div>
               <div className="flex items-center">

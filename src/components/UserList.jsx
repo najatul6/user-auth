13863c3508de93321928760ea084120f92021504
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-09-18 22:22:32 +0600

    remove useLoader form user list component

--- src/components/UserList.jsx
+++ src/components/UserList.jsx
@@ -1,7 +1,5 @@
-import { useLoaderData } from "react-router-dom"
 
 const UserList = () => {
-    const user = useLoaderData([])
   return (
     <div className="py-5 flex flex-col justify-center items-center px-3">
         <h1 className="text-2xl font-bold underline capitalize text-center py-2">All Users Here</h1>

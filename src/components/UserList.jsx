4d2a10fdda107c1c2ed97b8a82ec5b0936c319d2
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-09-18 22:18:00 +0600

    add some padding in right left and up down. also align all property in center

--- src/components/UserList.jsx
+++ src/components/UserList.jsx
@@ -1,7 +1,7 @@
 
 const UserList = () => {
   return (
-    <div>
+    <div className="py-5 flex flex-col justify-center items-center px-3">
         <h1 className="text-2xl font-bold underline capitalize text-center py-2">All Users Here</h1>
     </div>
   )

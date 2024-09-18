789a7e7fd54a03f82206ea6e597a60cedb6d51f5
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-09-18 22:18:50 +0600

    create a table in user list

--- src/components/UserList.jsx
+++ src/components/UserList.jsx
@@ -3,6 +3,11 @@ const UserList = () => {
   return (
     <div className="py-5 flex flex-col justify-center items-center px-3">
         <h1 className="text-2xl font-bold underline capitalize text-center py-2">All Users Here</h1>
+        <div>
+            <table>
+                
+            </table>
+        </div>
     </div>
   )
 }

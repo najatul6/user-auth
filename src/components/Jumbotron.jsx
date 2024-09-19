6fab0ac261082635db0bdf2e7a86a9fd13fd0723
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-09-19 22:24:18 +0600

    fix eslint problem

--- src/components/Jumbotron.jsx
+++ src/components/Jumbotron.jsx
@@ -1,37 +1,37 @@
 
 const Jumbotron = () => {
   return (
-    <div class="max-w-6xl mx-auto font-[sans-serif]">
-    <div class="bg-gray-50 max-sm:px-8 px-12 py-8 w-full rounded">
-      <h1 class="text-4xl font-extrabold text-gray-800">Tailwind CSS Jumbotron</h1>
-      <div class="mt-4">
-        <p class=" text-sm text-gray-800 leading-relaxed">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis accumsan, nunc et
+    <div className="max-w-6xl mx-auto font-[sans-serif]">
+    <div className="bg-gray-50 max-sm:px-8 px-12 py-8 w-full rounded">
+      <h1 className="text-4xl font-extrabold text-gray-800">Tailwind CSS Jumbotron</h1>
+      <div className="mt-4">
+        <p className=" text-sm text-gray-800 leading-relaxed">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis accumsan, nunc et
           tempus blandit, metus mi consectetur nibh, a pharetra felis turpis vitae ligula. Etiam laoreet velit nec neque
           ultrices, non consequat mauris tincidunt.</p>
       </div>
 
-      <button type="button" class="mt-6 px-6 py-3 rounded text-white text-sm tracking-wider border-none outline-none bg-blue-600 hover:bg-blue-700">Get started now</button>
+      <button type="button" className="mt-6 px-6 py-3 rounded text-white text-sm tracking-wider border-none outline-none bg-blue-600 hover:bg-blue-700">Get started now</button>
     </div>
 
-    <div class="grid md:grid-cols-2 gap-4 mt-4">
-      <div class="bg-[#333] text-white max-sm:px-8 px-12 py-8 w-full rounded">
-        <h2 class="text-2xl font-bold">Dark Card</h2>
-        <div class="mt-4">
-          <p class="text-sm leading-relaxed">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis accumsan, nunc et
+    <div className="grid md:grid-cols-2 gap-4 mt-4">
+      <div className="bg-[#333] text-white max-sm:px-8 px-12 py-8 w-full rounded">
+        <h2 className="text-2xl font-bold">Dark Card</h2>
+        <div className="mt-4">
+          <p className="text-sm leading-relaxed">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis accumsan, nunc et
             tempus blandit, metus mi consectetur nibh, a pharetra felis turpis vitae ligula. Etiam laoreet velit nec neque
             ultrices, non consequat mauris tincidunt.</p>
         </div>
-        <button type="button" class="mt-6 px-6 py-3 rounded text-sm tracking-wider border-none outline-none bg-gray-600 hover:bg-gray-700">Learn more</button>
+        <button type="button" className="mt-6 px-6 py-3 rounded text-sm tracking-wider border-none outline-none bg-gray-600 hover:bg-gray-700">Learn more</button>
       </div>
 
-      <div class="bg-gray-50 max-sm:px-8 px-12 py-8 w-full rounded">
-        <h2 class="text-gray-800 text-2xl font-bold">Gray Card</h2>
-        <div class="mt-4">
-          <p class="text-sm leading-relaxed">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis accumsan, nunc et
+      <div className="bg-gray-50 max-sm:px-8 px-12 py-8 w-full rounded">
+        <h2 className="text-gray-800 text-2xl font-bold">Gray Card</h2>
+        <div className="mt-4">
+          <p className="text-sm leading-relaxed">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis accumsan, nunc et
             tempus blandit, metus mi consectetur nibh, a pharetra felis turpis vitae ligula. Etiam laoreet velit nec neque
             ultrices, non consequat mauris tincidunt.</p>
         </div>
-        <button type="button" class="mt-6 px-6 py-3 rounded text-sm text-gray-800 tracking-wider border-none outline-none bg-gray-200 hover:bg-gray-300">Learn more</button>
+        <button type="button" className="mt-6 px-6 py-3 rounded text-sm text-gray-800 tracking-wider border-none outline-none bg-gray-200 hover:bg-gray-300">Learn more</button>
       </div>
     </div>
   </div>

474f403e91b4f4c8825111dd194ac80054a66841
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-09-19 22:15:39 +0600

    add padding class in testimonial section

--- src/components/Testimonial.jsx
+++ src/components/Testimonial.jsx
@@ -1,6 +1,6 @@
 const Testimonial = () => {
   return (
-    <div className="grid sm:grid-cols-2 items-center gap-8 font-[sans-serif] max-w-5xl max-sm:max-w-sm mx-auto p-4">
+    <div className="grid sm:grid-cols-2 items-center gap-8 py-16 max-w-5xl max-sm:max-w-sm mx-auto p-4">
     <div className="sm:h-[400px]">
       <img src="https://readymadeui.com/team-2.webp" className="w-full h-full object-contain" />
     </div>

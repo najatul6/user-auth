4a006345c0ac8acc5eb6ea0f1f7b13c75d912988
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-09-20 23:42:32 +0600

    text fixing

--- src/pages/Error10 .jsx
+++ src/pages/Error10 .jsx
@@ -10,8 +10,8 @@ const Error10 = () => {
             </div>
             <div className="w-full lg:w-1/2">
                 <h1 className="py-4 text-3xl lg:text-4xl font-extrabold text-gray-800">Looks like you&#39;ve found the doorway to the great nothing</h1>
-                <p className="py-4 text-base text-gray-800">The content you&#39;re looking for doesn&#39;t exist. Either it was removed, or you mistyped the link.</p>
-                <p className="py-2 text-base text-gray-800">Sorry about that! Please visit our hompage to get where you need to go.</p>
+                <p className="py-4 text-base text-gray-800">The content you&#39;re looking for dosen&#39;t exist. Either it was removed, or you mistyped the link.</p>
+                <p className="py-2 text-base text-gray-800">Sorry about that! Please visit our home page to get where you need to go.</p>
                 <Link to="/">
                 <button className="w-full lg:w-auto my-4 border rounded-md px-1 sm:px-16 py-5 bg-indigo-600 text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-700 focus:ring-opacity-50">Go back to Homepage</button>
                 </Link>

ee06b6e68ce577947630a8b62983f27fd845ec20
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-09-20 23:52:46 +0600

    Change size for images

--- src/pages/Error10 .jsx
+++ src/pages/Error10 .jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 const Error10 = () => {
     return (
         <div className="flex items-center flex-col justify-center lg:flex-row py-28 px-6 md:px-24 md:py-20 lg:py-32 gap-16 lg:gap-28">
-            <div className="w-full lg:w-1/2">
+            <div className="w-full lg:w-2/2">
                 <img className="hidden lg:block" src="https://i.ibb.co/v30JLYr/Group-192-2.png" alt="Error page Design" />
                 <img className="hidden md:block lg:hidden" src="https://i.ibb.co/c1ggfn2/Group-193.png" alt="Error page Design" />
                 <img className="md:hidden" src="https://i.ibb.co/8gTVH2Y/Group-198.png" alt="Error page icon" />

74fff6f29ec4551aca23addc18b924f3deb4fa0c
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-09-21 19:14:15 +0600

    if user created successfully then show toast message

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -3,6 +3,8 @@ import { useState } from "react";
 import { IoEye, IoEyeOff } from "react-icons/io5";
 import { Link } from "react-router-dom";
 import auth from "../Firebase/firebase.config";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const Register = () => {
   const [isShow, setIsShow] = useState(false);
@@ -16,6 +18,7 @@ const Register = () => {
   createUserWithEmailAndPassword(auth, email, password)
   .then((result) => {
     console.log(result.user);
+    toast("User Created Success!")
   })
   .catch((error) => {
     console.log(error.message);
@@ -46,6 +49,7 @@ const Register = () => {
   }
   return (
     <div className="bg-gray-50 flex items-center md:h-screen p-4">
+      <ToastContainer />
       <div className="w-full max-w-4xl max-md:max-w-xl mx-auto">
         <div className="bg-white grid md:grid-cols-2 gap-16 w-full sm:p-8 p-6 shadow-md rounded-md overflow-hidden">
           <div className="max-md:order-1 space-y-6">

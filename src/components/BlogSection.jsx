b1e1e3ad41162a0471a43482a4cdab80b19eaade
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-09-22 22:14:06 +0600

    Finish project with authentication credentials and password for authentication with the application

--- src/components/BlogSection.jsx
+++ src/components/BlogSection.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom"
 
 const BlogSection = () => {
   return (
@@ -10,7 +11,7 @@ const BlogSection = () => {
           <div className="bg-white cursor-pointer rounded overflow-hidden shadow-[0_2px_10px_-3px_rgba(6,81,237,0.3)] relative top-0 hover:-top-2 transition-all duration-300">
             <img src="https://readymadeui.com/Imagination.webp" alt="Blog Post 1" className="w-full h-60 object-cover" />
             <div className="p-6">
-              <span className="text-sm block text-gray-400 mb-2">10 FEB 2023 | BY NAJATUL ISLAM</span>
+              <span className="text-sm block text-gray-400 mb-2">10 FEB 2023 | BY <Link to="https://najatul-islam.vercel.app">NAJATUL ISLAM</Link></span>
               <h3 className="text-xl font-bold text-gray-800">A Guide to Igniting Your Imagination</h3>
               <hr className="my-4" />
               <p className="text-gray-400 text-sm">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis accumsan, nunc et tempus blandit, metus mi consectetur felis turpis vitae ligula.</p>

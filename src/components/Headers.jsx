b0a333c57eee2e43268b080df7a02ac23f74673e
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-09-22 23:06:25 +0600

    add in one div for some conditions

--- src/components/Headers.jsx
+++ src/components/Headers.jsx
@@ -37,19 +37,25 @@ const Headers = () => {
         </nav>
 
         {/* Login/Sign-up Buttons */}
-        <div className="flex space-x-3">
-          <NavLink to="/login">
-            <button className="px-4 py-2 text-sm rounded-full font-bold text-white border-2 border-[#FFA726] bg-[#FFA726] transition-all ease-in-out duration-300 hover:bg-transparent hover:text-[#FFA726]">
-              Login
-            </button>
-          </NavLink>
-          <NavLink to="/register">
-            <button className="px-4 py-2 text-sm rounded-full font-bold text-white border-2 border-[#FFA726] bg-[#FFA726] transition-all ease-in-out duration-300 hover:bg-transparent hover:text-[#FFA726]">
-              Sign up
-            </button>
-          </NavLink>
+        <div>
+          <div className="flex space-x-3">
+            <NavLink to="/login">
+              <button className="px-4 py-2 text-sm rounded-full font-bold text-white border-2 border-[#FFA726] bg-[#FFA726] transition-all ease-in-out duration-300 hover:bg-transparent hover:text-[#FFA726]">
+                Login
+              </button>
+            </NavLink>
+            <NavLink to="/register">
+              <button className="px-4 py-2 text-sm rounded-full font-bold text-white border-2 border-[#FFA726] bg-[#FFA726] transition-all ease-in-out duration-300 hover:bg-transparent hover:text-[#FFA726]">
+                Sign up
+              </button>
+            </NavLink>
+          </div>
           <div className="rounded-full border border-[#FFA726]">
-            <img src="https://readymadeui.com/cardImg.webp" alt="Profile picture" className="w-20 h-20" />
+            <img
+              src="https://readymadeui.com/cardImg.webp"
+              alt="Profile picture"
+              className="w-20 h-20"
+            />
           </div>
         </div>
 

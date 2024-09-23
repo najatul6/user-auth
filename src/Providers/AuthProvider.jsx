f947bb88414785836be857e516683354e6c7d241
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-09-23 22:04:03 +0600

    setup auth providers

--- src/Providers/AuthProvider.jsx
+++ src/Providers/AuthProvider.jsx
@@ -1,12 +1,23 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
+import PropTypes from "prop-types";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import auth from "../Firebase/firebase.config";
 
 export const AuthContext = createContext(null);
 
 const AuthProvider = ({ children }) => {
-  const authInfo = { users };
+  const [user,setUser]=useState(null)
+  const createUser=(email,password)=>{
+    return createUserWithEmailAndPassword(auth,email,password)
+  }
+  const authInfo = { user,createUser };
   return (
     <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
   );
+};
+
+AuthProvider.propTypes = {
+  children: PropTypes.node.isRequired,
 };
 
 export default AuthProvider;

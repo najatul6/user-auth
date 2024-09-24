df0d7c2ccc3f3c39813cbe9916f8a902ad8d6e5d
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-09-24 19:30:57 +0600

    install react-spinners

--- src/Routes/PrivateRoute.jsx
+++ src/Routes/PrivateRoute.jsx
@@ -4,7 +4,12 @@ import { Navigate } from "react-router-dom"
 import PropTypes from "prop-types";
 
 const PrivateRoute = ({children}) => {
-    const {user}=useContext(AuthContext)
+    const {user,loading}=useContext(AuthContext)
+
+    if(loading){
+        return <div>Loading...</div>
+    }
+
     if(user){
         return children
     }

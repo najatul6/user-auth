9031f21d22326263fa78aca2db9062fb7b632f02
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-09-24 19:26:14 +0600

    create loading information functions

--- src/Providers/AuthProvider.jsx
+++ src/Providers/AuthProvider.jsx
@@ -12,27 +12,34 @@ export const AuthContext = createContext(null);
 
 const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
+  const [loading,setLoading] = useState(true);
+
   const createUser = (email, password) => {
+    setLoading(true)
     return createUserWithEmailAndPassword(auth, email, password);
   };
+
   const signInUser = (email, password) => {
+    setLoading(true)
     return signInWithEmailAndPassword(auth, email, password);
   };
 
   const logOut = () => {
+    setLoading(true)
     return signOut(auth);
   };
 
   useEffect(() => {
     const unSubscribe = onAuthStateChanged(auth, (currentUser) => {
-      setUser(currentUser);
+      setUser(currentUser); 
+      setLoading(false)
     });
     return () => {
       unSubscribe();
     };
   }, []);
 
-  const authInfo = { user, createUser, signInUser, logOut };
+  const authInfo = { user,loading, createUser, signInUser, logOut };
   return (
     <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
   );

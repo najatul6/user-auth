0d8a7fa1c633eecd393558a60232a5dcd755fafe
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-09-24 19:19:23 +0600

    create private route for some candidate

---
+++ src/Routes/PrivateRoute.jsx
@@ -0,0 +1,18 @@
+import { useContext } from "react"
+import { AuthContext } from "../Providers/AuthProvider"
+import { Navigate } from "react-router-dom"
+import PropTypes from "prop-types";
+
+const PrivateRoute = ({children}) => {
+    const {user}=useContext(AuthContext)
+    if(user){
+        return children
+    }
+  return <Navigate to="/login"/>
+}
+
+PrivateRoute.propTypes = {
+    children: PropTypes.node.isRequired
+}
+
+export default PrivateRoute
